cf1a5c72b9a81d08d662f7fdd846d89809392edf
Sarah Lee <sarahselee223@gmail.com>
2021-03-02 22:00:56 -0800

    add viewControllers & images

--- Recipe1/ViewController.swift
+++ Recipe1/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let array = ["item1", "item2", "item3"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.rowHeight = 70
+        tableView.backgroundView = UIImageView(image: UIImage(named: "cozy copy"))
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.navigationBar.alpha = 0.5
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // If cells go out of view, we can reuse the cells
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath)
+        if(indexPath.item % 2 == 0){
+            cell.backgroundColor = UIColor.white.withAlphaComponent(0.1)
+            cell.textLabel?.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        } else {
+            cell.backgroundColor = UIColor.white.withAlphaComponent(0.2)
+            cell.textLabel?.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        }
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.text = array[indexPath.item]
+        return cell
+    }
 }
 
